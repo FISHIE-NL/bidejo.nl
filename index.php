097ffd0c88e0a08e9a6306570cf17ba0d552d545
BjornVis010 <9018609@student.zadkine.nl>
2025-02-01 05:39:33 +0100

    opzet website

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bidejo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php"><img src="img/Bidejo.png" alt="logo Bidejo" class="logo_bidejo"></a>
+        <ul>
+            <a href="#about">over ons</a>
+            <a href="#contact">contact</a>
+        </ul>
+    </nav>
+    <div class="topnav">
+        <ul>
+            <li><a href="#feestdagen">feestdagen</a></li>
+            <li><a href="#verjaardag">verjaardag</a></li>
+            <li><a href="#kerst">kerst</a></li>
+            <li><a href="#valentijn">valentijn</a></li>
+            <li><a href="#trouwen">trouwen</a></li>
+        </ul>
+        <input type="text" placeholder="Zoek..">
+    </div>
+</body>
+</html>
