e70b932061a8737a9abc297617af855057182f8a
BjornVis010 <9018609@student.zadkine.nl>
2025-02-02 02:17:04 +0100

    added icons and a new font

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bidejo</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="img/Bidejo.png">
+
 </head>
 <body>
     <nav>
         <a href="index.php"><img src="img/Bidejo.png" alt="logo Bidejo" class="logo_bidejo"></a>
         <ul>
             <a href="#about">over ons</a>
-            <a href="#contact">contact</a>
         </ul>
+        <a class="phoneIcon" href="tel:+31611808246"><img src="icon/call.png" alt="phone" style="width: 4rem; height: auto;"></a>
+        <a href="mailto:bjornvisimp@gmail.com"><img class="emailIcon" src="icon/email.png" alt="email" style="width: 4rem; height: auto;"></a>
     </nav>
     <div class="topnav">
         <ul>
